8040609b0c65a1c7d1a72577600e790c4d969a91
Rafail Dimchev Stankov <rafail.stankov@rwth-aachen.de>
2021-09-13 01:30:20 +0200

    trying to catch data from android

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,13 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello');
 });
 
+app.post('/data-from-android', (req: Request, res: Response) => {
+  if (req.body) {
+    console.log(req.body);
+  }
+  res.sendStatus(200);
+});
+
 app.listen(process.env.PORT || port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
